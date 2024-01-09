abc1fadbf2b1bf067eb7781cca38fccb3218f639
nazzout <140650470+nazzout@users.noreply.github.com>
2024-01-08 21:12:10 -0800

    birthdayStire final

--- index.js
+++
@@ -1,9 +0,0 @@
-const giftImg = document.querySelector('.gift-img');
-
-giftImg.addEventListener('touchstart', function() {
-    this.classList.add('active');
-});
-
-giftImg.addEventListener('touchend', function() {
-    this.classList.remove('active');
-});
